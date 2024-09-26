8873d0a1c176188be9153db3d17b0dcd5b45c404
trubsee <trubsee@gmail.com>
2024-09-26 13:34:21 +1000

    test rename

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Eigen::MatrixXd m1{{3, 2.5}, {-1, 0}};
+    Eigen::MatrixXd m1{{2, 2.5}, {-1, 0}};
     Eigen::MatrixXd m2{{1, 1}, {0, 1}};
     std::cout << m1 * m2 << std::endl;
 }
