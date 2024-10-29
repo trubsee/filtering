f17397f02457e94d2368d1caf39915524d7a9f92
trubsee <trubsee@gmail.com>
2024-10-30 10:41:25 +1100

    initial benchmarking

--- common/statistics.hpp
+++ common/statistics.hpp
@@ -9,13 +9,9 @@ namespace Common {
 static Eigen::MatrixXd CalculateCovariance(const Eigen::MatrixXd& X, const Eigen::MatrixXd& Y)
 {
     ASSERT(X.rows() == Y.rows());
-
     const auto n = X.rows();
-    const auto meanX = X.colwise().mean();
-    const auto meanY = Y.colwise().mean();
-
-    const auto centredX = X.rowwise() - meanX;
-    const auto centredY = Y.rowwise() - meanY;
+    const auto centredX = X.rowwise() - X.colwise().mean();
+    const auto centredY = Y.rowwise() - Y.colwise().mean();
 
     return (centredX.transpose() * centredY) / (n - 1);
 }
---
+++ filters/benchmark/stochasticEKFBenchmark.cpp
@@ -0,0 +1,46 @@
+#include "benchmark/benchmark.h"
+
+#include "common/randomSample.hpp"
+#include "filters/stochasticEKF.ipp"
+#include "stochasticModels/linearFactoryMethods.hpp"
+
+namespace Filters::Benchmark {
+
+static void BenchmarkStochasticEKF(benchmark::State& state)
+{
+    const double initial{0.};
+    const double drift{0.5};
+    const double noise{0.1};
+    const auto stateModel{
+        StochasticModels::CreateRandomWalk(Eigen::MatrixXd{{drift}})
+    };
+    const auto obsModel{
+        StochasticModels::CreateRandomWalk(Eigen::MatrixXd{{noise}})
+    };
+
+    StochasticEKF<500> ekf {
+        Eigen::MatrixXd{{initial}},
+        Eigen::MatrixXd{{1}},
+        stateModel,
+        obsModel
+    };
+
+    const unsigned ITER{1000};
+
+    for (auto _ : state)
+    {
+        auto hiddenState = initial + Common::SampleNormal() * std::sqrt(drift);
+
+        for (unsigned i = 0; i < ITER; ++i)
+        {
+            // generate fake obs
+            auto obs = hiddenState + Common::SampleNormal() * std::sqrt(noise);
+            ekf.Update(Eigen::VectorXd{{obs}});
+        }
+    }
+}
+BENCHMARK(BenchmarkStochasticEKF)->Arg(10);
+
+}
+
+BENCHMARK_MAIN();
